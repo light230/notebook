993ae2d011076a1a3954a0a3778c25d1bbbab158
light230 <90103997+light230@users.noreply.github.com>
2021-09-29 01:50:28 +0700

    addp299

---
+++ wp-content/themes/authors-notepad-sxc/js/frontend.js
@@ -0,0 +1,13 @@
+/*
+ * Theme JavaScript and JQuery code for the frontend pages of website
+ *
+ * Copyright (c) 2021, Space X-Chimp ( https://www.spacexchimp.com ).
+ * All Rights Reserved.
+ */
+
+jQuery(document).ready(function($) {
+
+    // On the yellow sticker on the right side of the notepad, the submenu feature styling
+    $('#sidebar .yellow-sticker-middle ul .sub-menu:has(a[aria-current])').css('display', 'block');
+
+});
